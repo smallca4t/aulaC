b0bcee009bea417b7e0f290597bbf9293f7e4fbf
smallca4t <142899643+smallca4t@users.noreply.github.com>
2023-11-21 19:21:35 -0300

    Delete aulinhac directory

--- aulinhac/teste2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int celsius;
-    
-    printf( "Digite a temperatura em Celsius: ");
-    scanf( "%d", &celsius);
-
-    printf("%d °C = %d °F", celsius, celsius * 9/5 + 32);
-}
